21e39894016357a1f79a7ff6fc3d44eac2a70211
Lukas Schauhuber <35468672+luggaesch@users.noreply.github.com>
2021-12-22 11:51:55 +0100

    lösungsvorschlag ergänzt

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,12 @@ public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog("Rex", 5, "German Shepherd");
         Cat cat = new Cat("Fluffy", 3, true);
+        System.out.println(willGetAlong(dog, cat));
     }
-    /*
-        Ergänze hier die neue Methode 'willGetAlong'.
-     */
+
+    private static boolean willGetAlong(Animal animal1, Animal animal2) {
+        boolean bothDogs = animal1 instanceof Dog && animal2 instanceof Dog;
+        boolean bothCats = animal1 instanceof Cat && animal2 instanceof Cat;
+        return bothDogs || bothCats;
+    }
 }
