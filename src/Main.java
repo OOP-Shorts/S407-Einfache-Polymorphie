f4627c3a3cb39bfe5dd6659efe0aa38ce560c0f6
Lukas Schauhuber <35468672+luggaesch@users.noreply.github.com>
2021-12-22 11:50:06 +0100

    startercode hinzugefügt

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Rex", 5, "German Shepherd");
+        Cat cat = new Cat("Fluffy", 3, true);
+    }
+    /*
+        Ergänze hier die neue Methode 'willGetAlong'.
+     */
+}
